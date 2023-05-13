83cc7faba5b3d65de16183cd39433c66bd9cdfd8
Tien <ngotienpts@gmail.com>
2023-05-13 11:47:27 +0700

    13/5

--- js/main.js
+++ js/main.js
@@ -1,86 +1,117 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // back top
-  var backTop = document.querySelector("#back-top");
+    // get element
+    function getElement(selector) {
+        var element = document.querySelector(selector);
+        return element;
+    }
+    // get all element
+    function getAllElement(selector) {
+        var elements = document.querySelectorAll(selector);
+        return elements;
+    }
 
-  // width document
-  var widthDoc = document.querySelector("body");
-
-  const app = {
-    // su ly cac su kien
-    handleEvent: function () {
-      const _this = this;
-
-      // when click back top
-      if (backTop) {
-        backTop.onclick = function () {
-          document.body.scrollTop = 0;
-          document.documentElement.scrollTop = 0;
+    // add show and scroll body hidden
+    function showElementHiddenBody(item, action) {
+        item.onclick = function () {
+            this.classList.toggle(action);
+            document.querySelector("body").classList.toggle("hidden");
         };
-      }
-    },
+    }
+    // add show item
+    function showItem(item, action) {
+        item.onclick = function () {
+            this.classList.toggle(action);
+        };
+    }
+    // add show items
+    function showItemList(items, action) {
+        items.forEach((item) => {
+            item.onclick = function () {
+                this.classList.toggle(action);
+            };
+        });
+    }
+    // body
+    var doc = getElement("body");
+    // back top
+    var backTop = getElement("#back-top");
 
-    // media
-    sliderMedia: function () {
-      var swiper = new Swiper(".mySwiperMediaSecondary", {
-        slidesPerView: 2,
-        slidesPerGroup: 1,
-        freeMode: true,
-        watchSlidesProgress: true,
-        direction: "vertical",
-        mousewheel: true,
-        scrollbar: {
-          el: ".swiper-scrollbar",
-        },
-        // breakpoints: {
-        //   768: {
-        //     slidesPerView: 2,
-        //     spaceBetween: 0,
-        //     slidesPerGroup: 1,
-        //   },
-        // },
-      });
-      var swiper2 = new Swiper(".mySwiperMediaPrimary", {
-        direction: "vertical",
-        slidesPerView: 1,
-        spaceBetween: 30,
-        mousewheel: true,
-        thumbs: {
-          swiper: swiper,
-        },
-      });
-    },
-    // scroll top
-    scrollFunc: function () {
-      if (backTop) {
-        if (
-          document.body.scrollTop > 300 ||
-          document.documentElement.scrollTop > 300
-        ) {
-          backTop.style.opacity = 1;
-          backTop.style.visibility = "visible";
-        } else {
-          backTop.style.opacity = 0;
-          backTop.style.visibility = "hidden";
-        }
-      }
-    },
+    const app = {
+        // su ly cac su kien
+        handleEvent: function () {
+            const _this = this;
 
-    // window scroll
-    windowScroll: function () {
-      var _this = this;
-      window.onscroll = function () {
+            // when click back top
+            if (backTop) {
+                backTop.onclick = function () {
+                    document.body.scrollTop = 0;
+                    document.documentElement.scrollTop = 0;
+                };
+            }
+        },
+
+        // media
+        sliderMedia: function () {
+            var swiper = new Swiper(".mySwiperMediaSecondary", {
+                slidesPerView: 2,
+                slidesPerGroup: 1,
+                freeMode: true,
+                watchSlidesProgress: true,
+                direction: "vertical",
+                mousewheel: true,
+                scrollbar: {
+                    el: ".swiper-scrollbar",
+                },
+                // breakpoints: {
+                //   768: {
+                //     slidesPerView: 2,
+                //     spaceBetween: 0,
+                //     slidesPerGroup: 1,
+                //   },
+                // },
+            });
+            var swiper2 = new Swiper(".mySwiperMediaPrimary", {
+                direction: "vertical",
+                slidesPerView: 1,
+                spaceBetween: 30,
+                mousewheel: true,
+                thumbs: {
+                    swiper: swiper,
+                },
+            });
+        },
         // scroll top
-        _this.scrollFunc();
-      };
-    },
-    // khoi tao function start
-    start: function () {
-      // su ly cac su kien
-      this.handleEvent();
-      // window scroll
-      this.windowScroll();
-    },
-  };
+        scrollFunc: function () {
+            if (backTop) {
+                if (
+                    document.body.scrollTop > 300 ||
+                    document.documentElement.scrollTop > 300
+                ) {
+                    backTop.style.opacity = 1;
+                    backTop.style.visibility = "visible";
+                } else {
+                    backTop.style.opacity = 0;
+                    backTop.style.visibility = "hidden";
+                }
+            }
+        },
 
-  app.start();
+        // window scroll
+        windowScroll: function () {
+            var _this = this;
+            window.onscroll = function () {
+                // scroll top
+                _this.scrollFunc();
+            };
+        },
+        // khoi tao function start
+        start: function () {
+            // su ly cac su kien
+            this.handleEvent();
+            // window scroll
+            this.windowScroll();
+        },
+    };
+
+    app.start();
 });
