01f5638d4509b523d1572db3df3a2d5bb7bd7b25
Tien <ngotienpts@gmail.com>
2023-05-16 17:26:13 +0700

    16/5

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelector("body").classList.toggle("hidden");
         };
     }
+
     // add show item
     function showItem(item, action) {
         item.onclick = function () {
@@ -36,6 +37,10 @@ document.addEventListener("DOMContentLoaded", function () {
     // back top
     var backTop = getElement("#back-top");
 
+    // show menu pc
+    var menuPc = getElement(".js_clickShowMenu");
+    var closeMenuPc = getElement(".js_closeMenu");
+
     const app = {
         // su ly cac su kien
         handleEvent: function () {
@@ -46,6 +51,20 @@ document.addEventListener("DOMContentLoaded", function () {
                 backTop.onclick = function () {
                     document.body.scrollTop = 0;
                     document.documentElement.scrollTop = 0;
+                };
+            }
+
+            // show menu pc
+            if (menuPc) {
+                var item = menuPc.parentElement.querySelector(".js_showMenu");
+                menuPc.onclick = function () {
+                    item.classList.add("show");
+                    document.querySelector("body").classList.add("hidden");
+                };
+
+                closeMenuPc.onclick = function () {
+                    item.classList.remove("show");
+                    document.querySelector("body").classList.remove("hidden");
                 };
             }
         },
@@ -75,22 +94,85 @@ document.addEventListener("DOMContentLoaded", function () {
         // slider six item
         sliderSixItems: function () {
             var swiper = new Swiper(".js_sixItems", {
-                slidesPerView: 6,
-                spaceBetween: 30,
+                slidesPerView: 2.3,
+                spaceBetween: 16,
+                slidesPerGroup: 2,
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                breakpoints: {
+                    640: {
+                        slidesPerView: 2,
+                        slidesPerGroup: 2,
+                        spaceBetween: 16,
+                    },
+                    768: {
+                        slidesPerView: 4.3,
+                        slidesPerGroup: 4,
+                        spaceBetween: 16,
+                    },
+                    1024: {
+                        slidesPerView: 6,
+                        spaceBetween: 16,
+                        slidesPerGroup: 6,
+                    },
+                    1200: {
+                        slidesPerView: 6,
+                        spaceBetween: 25,
+                        slidesPerGroup: 6,
+                    },
+                },
+            });
+        },
+        // slider three item
+        sliderThreeItems: function () {
+            var swiper = new Swiper(".js_threeItems", {
+                slidesPerView: 1.2,
+                spaceBetween: 16,
+                slidesPerGroup: 1,
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                breakpoints: {
+                    640: {
+                        slidesPerView: 1,
+                        slidesPerGroup: 1,
+                        spaceBetween: 16,
+                    },
+                    768: {
+                        slidesPerView: 2.2,
+                        spaceBetween: 16,
+                        slidesPerGroup: 2,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 16,
+                        slidesPerGroup: 3,
+                    },
+                    1200: {
+                        slidesPerView: 3,
+                        spaceBetween: 25,
+                        slidesPerGroup: 3,
+                    },
+                },
             });
         },
         // scroll top
         scrollFunc: function () {
             if (backTop) {
                 if (
-                    document.body.scrollTop > 300 ||
-                    document.documentElement.scrollTop > 300
+                    document.body.scrollTop > 600 ||
+                    document.documentElement.scrollTop > 600
                 ) {
-                    backTop.style.opacity = 1;
-                    backTop.style.visibility = "visible";
+                    // backTop.style.opacity = 1;
+                    // backTop.style.visibility = "visible";
+                    backTop.style.top = "2.25rem";
                 } else {
-                    backTop.style.opacity = 0;
-                    backTop.style.visibility = "hidden";
+                    // backTop.style.opacity = 0;
+                    // backTop.style.visibility = "hidden";
+                    backTop.style.top = "-2.25rem";
                 }
             }
         },
@@ -113,6 +195,8 @@ document.addEventListener("DOMContentLoaded", function () {
             this.sliderPrimary();
             // slider six item
             this.sliderSixItems();
+            // slider three item
+            this.sliderThreeItems();
         },
     };
 
